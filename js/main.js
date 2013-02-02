b48c9304aa581173563a28b016a373bb63e2fea4
Robert Pyke <robert.j.pyke@gmail.com>
2013-02-02 21:22:20 +1000

    Updated site to contain to use 'cards' interface

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+$('.card').hover(
+  function(){
+    var me = this;
+    $('.card').each( function() {
+      if(this != me) {
+        $(this).addClass('unfocused');
+      }
+    });
+  },
+  function(){
+    var me = this;
+    $('.card').each( function() {
+      if(this != me) {
+        $(this).removeClass('unfocused');
+      }
+    });
+  }
+);
